1055057488ba5897dcfb0471cc0c2dc05aa00f87
alekgr <alek.grigorian@gmail.com>
2022-11-10 10:20:29 -0600

    added some bolding and titles  to reporting

--- wimh/report.py
+++ wimh/report.py
@@ -4,32 +4,39 @@ import vars
 def print_file_info():
     ''' This is out of file information '''
 
+    BOLD = '\033[1m'
+    END  = '\033[0m'
+
     if vars.option_all == True:
-        print(f"Number of Directories: {len(vars.directories)} ");
-        print(f"Number of Files {len(vars.files)}")
-        print(f"Number of Symbolic links {len(vars.symbolic_links)} ")
-        print(f"Number of Empty directories {len(vars.empty_dirs)} ")
-        print(f"Number of Images {len(vars.images)} ")
-        print(f"Number of Videos {len(vars.videos)} ")
-        print(f"Number of Audios {len(vars.audio)} ")
-        print(f"Number of Dot Files {len(vars.dot_files)} ")
-        print(f"Number of Dot Directories {len(vars.dot_directories)} ")
-        print(f"Number of C source files {len(vars.c_src)} ")
-        print(f"Number of C header files {len(vars.c_header)} ")
-        print(f"Number of C++ source files {len(vars.c_plus_src)} ")
-        print(f"Number of C++ header files {len(vars.c_plus_header)} ")
-        print(f"Number of Java source files {len(vars.java_src)} ")
-        print(f"Number of Java class files {len(vars.java_class)} ")
-        print(f"Number of Java archive  files {len(vars.java_archive)} ")
-        print(f"Number of Java native interface files {len(vars.java_native_interface)} ")
-        print(f"Number of css files {len(vars.css_src)} ")
-        print(f"Number of python source files {len(vars.python_src)} ")
-        print(f"Number of python bytecode files {len(vars.python_bytecode)} ")
-        print(f"Number of go source files {len(vars.go_src)} ")
-        print(f"Number of htm(l) source files {len(vars.html_src)} ")
-        print(f"Number of xml source files {len(vars.xml_src)} ")
-        print(f"Number of javascript source files {len(vars.javascript_src)} ")
-        print(f"Number of php source files {len(vars.php_src)} ")
+        print(BOLD + "General file Information *******" + END)
+        print(f"\tNumber of Directories: {len(vars.directories)} ");
+        print(f"\tNumber of Files {len(vars.files)}")
+        print(f"\tNumber of Symbolic links {len(vars.symbolic_links)} ")
+        print(f"\tNumber of Empty directories {len(vars.empty_dirs)} ")
+        print(f"\tNumber of Dot Files {len(vars.dot_files)} ")
+        print(f"\tNumber of Dot Directories {len(vars.dot_directories)} ")
+
+        print(BOLD + "Media *******" + END)
+        print(f"\tNumber of Images {len(vars.images)} ")
+        print(f"\tNumber of Videos {len(vars.videos)} ")
+        print(f"\tNumber of Audios {len(vars.audio)} ")
+        print(BOLD + "Project Source code *******" + END)
+        print(f"\tNumber of C source files {len(vars.c_src)} ")
+        print(f"\tNumber of C header files {len(vars.c_header)} ")
+        print(f"\tNumber of C++ source files {len(vars.c_plus_src)} ")
+        print(f"\tNumber of C++ header files {len(vars.c_plus_header)} ")
+        print(f"\tNumber of Java source files {len(vars.java_src)} ")
+        print(f"\tNumber of Java class files {len(vars.java_class)} ")
+        print(f"\tNumber of Java archive  files {len(vars.java_archive)} ")
+        print(f"\tNumber of Java native interface files {len(vars.java_native_interface)} ")
+        print(f"\tNumber of css files {len(vars.css_src)} ")
+        print(f"\tNumber of python source files {len(vars.python_src)} ")
+        print(f"\tNumber of python bytecode files {len(vars.python_bytecode)} ")
+        print(f"\tNumber of go source files {len(vars.go_src)} ")
+        print(f"\tNumber of htm(l) source files {len(vars.html_src)} ")
+        print(f"\tNumber of xml source files {len(vars.xml_src)} ")
+        print(f"\tNumber of javascript source files {len(vars.javascript_src)} ")
+        print(f"\tNumber of php source files {len(vars.php_src)} ")
 
     else:
             if vars.option_numberofdirectories == True:
